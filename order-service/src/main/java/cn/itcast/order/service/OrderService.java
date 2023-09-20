f739a83cb12d83bb5b570d1188a405f7df04cda9
umehara <a603751058@gmail.com>
2023-09-20 21:41:27 +0800

    feignでレモートコールする　完成

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,8 +1,10 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
+import com.sun.org.apache.xerces.internal.impl.io.UCSReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,15 +15,15 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
+
 
     public Order queryOrderById(Long orderId) {
         // 1.注文を検索
         Order order = orderMapper.findById(orderId);
 
-        //2.RestTemplateを利用し、httpリクエスト請求を発送する
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        //2.feignでレモートコール
+        User user = userClient.findById(order.getUserId());
 
         //3.userをorderに代入
         order.setUser(user);
@@ -29,4 +31,23 @@ public class OrderService {
         //4.返す
         return order;
     }
+
+
+    //    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.注文を検索
+//        Order order = orderMapper.findById(orderId);
+//
+//        //2.RestTemplateを利用し、httpリクエスト請求を発送する
+//        String url = "http://userservice/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//
+//        //3.userをorderに代入
+//        order.setUser(user);
+//
+//        //4.返す
+//        return order;
+//    }
 }
